243cdfb765490c6f5a8541b46924e131ed913fed
Aakash <srivastavaak1203@gmail.com>
2025-04-18 18:20:56 +0530

    delete server folder

--- server/controllers/user.js
+++
@@ -1,8 +0,0 @@
-const router =  require("express").Router();
-const { register, login, logout } = require("../services/user.js");
-
-router.post("/register", register);
-router.post("/login", login);
-router.post("/logout", logout);
-
-module.exports = router;
